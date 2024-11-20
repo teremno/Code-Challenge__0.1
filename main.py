7743a130cdd4f18d4bbbd9f2eb708b9e08a61715
teremno <teremnoua@gmail.com>
2024-11-20 23:27:29 +0100

    Вчився. Цікаво про взведення у степінь

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello Code Challenge")
+# print("Hello Code Challenge")
+
+'''c = int(input('ведіть число: '))
+b = int(input('ведіть число: '))
+print(c + b)'''
+
+a = 3
+b = 7
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(99 // 10)
+print(5 % 2)
+print(5642685428748974687 % 4)
+print(47644467417 % 1000)
+print(4 ** 4)
