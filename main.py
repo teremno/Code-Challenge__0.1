6189a45be4d1a35c9608ca5eb33033cb5d6732f0
teremno <teremnoua@gmail.com>
2024-11-22 00:27:47 +0100

    Далі вчусь. Що правда, мало часу виділяю на навчання

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 '''c = int(input('ведіть число: '))
 b = int(input('ведіть число: '))
-print(c + b)'''
+print(c + b)
 
 a = 3
 b = 7
@@ -16,3 +16,36 @@ print(5 % 2)
 print(5642685428748974687 % 4)
 print(47644467417 % 1000)
 print(4 ** 4)
+print(1 > 2)
+print(1 < 2)
+a = int(input("Ведіть число: "))
+if a == 1:
+    print('Ви ввели 1')
+else:
+    print('Ви ввели не 1')
+
+finger = int(input("Ведіть номер пальця: "))
+if finger == 1:
+    print('Великий палець')
+elif finger == 2:
+    print('Вказивний палець')
+elif finger == 3:
+    print('Середній палець')
+elif finger == 4:
+    print('Безімений палець')
+elif finger == 5:
+    print('Мізимець')
+else:
+    print('Неправильний номер пальця')
+
+a = int(input('Ведіть число: '))
+if a >= 1 and a <= 3:
+    print('Попали в діапазон')
+else:
+    print('Не попали в діапазон')'''
+
+a = int(input('Ведіть число: '))
+if a <= 1 or a >= 3:
+    print('Попали в діапазон')
+else:
+    print('Не попали в діапазон')
