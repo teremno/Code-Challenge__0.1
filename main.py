a80dfb6334b9cff91dd0044eeac87a0ca0a8ecb0
teremno <teremnoua@gmail.com>
2024-11-23 01:31:06 +0100

    Ох! Я трохипомучився з каоькулятором. Це поки що, є найважче, що я робив

--- main.py
+++ main.py
@@ -1,51 +1,119 @@
-# print("Hello Code Challenge")
+# Практика  1 уровень: 1, 2, 3, 4
+# 1
 
-'''c = int(input('ведіть число: '))
-b = int(input('ведіть число: '))
-print(c + b)
+# a = int(input('Ведіть число: '))
+# b = int(input('Ведіть число: '))
+# print(a ** b)
 
-a = 3
-b = 7
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-print(a // b)
-print(99 // 10)
-print(5 % 2)
-print(5642685428748974687 % 4)
-print(47644467417 % 1000)
-print(4 ** 4)
-print(1 > 2)
-print(1 < 2)
-a = int(input("Ведіть число: "))
-if a == 1:
-    print('Ви ввели 1')
+# 2
+
+# day = int(input('Ведіть число від 1 до 7: '))
+# if day == 1:
+#     print('Понеділок')
+# elif day == 2:
+#     print('Вівторок')
+# elif day == 3:
+#     print('Середа')
+# elif day == 4:
+#     print('Четвер')
+# elif day == 5:
+#     print('Пятниця')
+# elif day == 6:
+#     print('Субота')
+# elif day == 7:
+#     print('Неділя')
+# else:
+#     print('Ви хочете більше часу, на жаль його нема. Цінуйте час, який ще є!')
+
+# 3
+
+# number1 = int(input('Введіть число: '))
+# number2 = int(input('Введіть число: '))
+# if number1 > number2:
+#     print(number1)
+# else:
+#     print(number2)
+
+# 4
+
+# _deposit_usd = float(input('Введіть свій депозит в USD: '))
+# _kurs_uah_usd = float(input('Введіть курс USD до UAH: '))
+# your_depo = _deposit_usd * _kurs_uah_usd
+# print('Ваш депозит у гривнях ' + str(your_depo))
+
+# 2 уровень: 1, 2, 3
+# 1
+
+# _user_number = int(input('Введіть число: '))
+# if _user_number % 2 == 0:
+#     print('Парне число')
+# else:
+#     print('Непарне число')
+
+# 2
+
+# _numb_user_1 = int(input('Введіть число: '))
+# _numb_user_2 = int(input('Введіть число: '))
+# _numb_user_3 = int(input('Введіть число: '))
+#
+# if _numb_user_1 > _numb_user_2 and _numb_user_1 > _numb_user_3:
+#     print(_numb_user_1, 'Це найбільше число')
+# elif _numb_user_2 > _numb_user_3:
+#     print(_numb_user_2, 'Це найбільше число')
+# else:
+#     print(_numb_user_3, 'Це найбільше число')
+
+# 3
+
+# _min_sleep_ = int(input('Мінімальни час сну? '))
+# _max_sleep_ = int(input('Максимальний час сну? '))
+# _time_sleep_ = int(input('Скільки часу ви спите? '))
+#
+# if _time_sleep_ < _min_sleep_:
+#     print('Ви спите не достатньо!')
+# elif _time_sleep_ > _max_sleep_:
+#     print('Ви спите забагато!')
+# else:
+#     print('Ви спите добре!')
+
+# 3 уровень: 1, 2, 3
+
+# 1
+
+_num_1 = float(input('Ввести число: '))
+_num_2 = float(input('Ввести число: '))
+_math_simbol = input('Ввдіть символ: (+, -, /, *, mod, pow, div) ')
+
+if _math_simbol == '+':
+    result = _num_1 + _num_2
+
+elif _math_simbol == '-':
+    result = _num_1 - _num_2
+
+elif _math_simbol == '/':
+    if _num_2 != 0:
+        result = _num_1 / _num_2
+    else:
+        result = 'Ділення на нуль'
+elif _math_simbol == '*':
+    result = _num_1 * _num_2
+
+elif _math_simbol == 'mod':
+    if _num_2 != 0:
+        result = _num_1 % _num_2
+    else:
+        result = 'Ділення на нуль'
+
+elif _math_simbol == 'pow':
+    result = _num_1 ** _num_2
+
+elif _math_simbol == 'div':
+    if _num_2 != 0:
+        result = _num_1 // _num_2
+    else:
+        result = 'Ділення на нуль'
+
 else:
-    print('Ви ввели не 1')
+    result = 'Не вірний формат введення'
 
-finger = int(input("Ведіть номер пальця: "))
-if finger == 1:
-    print('Великий палець')
-elif finger == 2:
-    print('Вказивний палець')
-elif finger == 3:
-    print('Середній палець')
-elif finger == 4:
-    print('Безімений палець')
-elif finger == 5:
-    print('Мізимець')
-else:
-    print('Неправильний номер пальця')
-
-a = int(input('Ведіть число: '))
-if a >= 1 and a <= 3:
-    print('Попали в діапазон')
-else:
-    print('Не попали в діапазон')'''
-
-a = int(input('Ведіть число: '))
-if a <= 1 or a >= 3:
-    print('Попали в діапазон')
-else:
-    print('Не попали в діапазон')
+print('Результат ', result)
